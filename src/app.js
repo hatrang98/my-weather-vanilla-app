e76fe464827d99d045ed88d349ba077c05e01bd7
hatrang98 <67720505+hatrang98@users.noreply.github.com>
2022-04-24 11:53:46 +0700

    add Forecast API fix

--- src/app.js
+++ src/app.js
@@ -40,6 +40,42 @@ function showTemp(response) {
   let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
   let iconElement = document.querySelector("#weather-icon");
   iconElement.setAttribute("src", iconUrl);
+  //ad forecast
+  function showForecastDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = date.getDay();
+    return days[day];
+  }
+  function showForecast(response) {
+    console.log(response.data);
+    let forecast = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHTML =
+          forecastHTML +
+          `<div class="col-2 forecast-eachDay">
+              <span class="forecast-date">${showForecastDay(day.dt)}</span>
+              <br />
+              <img
+                src="http://openweathermap.org/img/wn/${
+                  day.weather[0].icon
+                }@2x.png"
+                class="forecast-icon"
+              />
+              <br />
+              <span class="temp-max">${Math.round(day.temp.max)}°</span>
+              <span class="temp-min">${Math.round(day.temp.min)}°</span>
+            </div>`;
+      }
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecast.innerHTML = forecastHTML;
+  }
+  let apiKey = "634e0f286e83523b2a964bf9f5ac1617";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 // add search
@@ -80,26 +116,3 @@ let degreeF = document.querySelector("#degreeF");
 degreeF.addEventListener("click", showDegreeF);
 let degreeC = document.querySelector("#degreeC");
 degreeC.addEventListener("click", showDegreeC);
-
-// add forecast
-let forecast = document.querySelector("#forecast");
-let forecastHTML = `<div class="row">`;
-let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-days.forEach(function (day) {
-  forecastHTML =
-    forecastHTML +
-    `<div class="col-2 forecast-eachDay">
-              <span class="forecast-date">${day}</span>
-              <br />
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-256/cloudy-weather-11-1147979.png"
-                width="20px"
-                class="forecast-icon"
-              />
-              <br />
-              <span class="temp-max">20°</span>
-              <span class="temp-min">10°</span>
-            </div>`;
-});
-forecastHTML = forecastHTML + `</div>`;
-forecast.innerHTML = forecastHTML;
